ac848acbe1c33a7be7226c9f33bfc06db214633e
Ben Schneedecker <brschneedecker@gmail.com>
2018-12-28 13:24:00 -0500

    Add logging to download.py

--- src/data/download.py
+++ src/data/download.py
@@ -5,7 +5,30 @@ Download HTML tables with QB season summary statistics from Pro Football
 Reference and Football Outsiders and store as .csv files
 """
 
-def get_all_seasons(bgn_yr: int, end_yr: int, base_html: str):
+import pandas as pd
+import re
+import os
+import logging
+
+def get_season(html_path: str):
+	"""
+	Download a single season of HTML table data and return DataFrame
+
+	Args:
+	  - html_path: String, path to page with HTML table data
+
+	Returns:
+	  - df: DataFrame with extracted data
+	"""
+	try:
+		df = pd.read_html(html_path)[0]
+	except Exception as err:
+		logger.warning("Unsuccessful download from {}".format(html_path))
+	else:
+		logger.info("Download from {} complete".format(html_path))
+		return df
+
+def get_all_seasons(bgn_yr: int, end_yr: int, base_html: str, outfile: str):
 	"""
 	Get all seasons of QB data for a given range of years
 
@@ -19,22 +42,42 @@ def get_all_seasons(bgn_yr: int, end_yr: int, base_html: str):
 	  - df: DataFrame object with downloaded data
 	"""
 
-	# Generate list of years
+	logger.info("Creating {}".format(outfile))
+
+	if os.path.exists(outfile):
+		logger.info("{} already exists, deleting".format(outfile))
+		os.remove(outfile)
+
+	# Generate list of years for which to download data
 	year_list = [year for year in range(bgn_yr, end_yr + 1)]
 
 	# Get data for all years and store in list of DataFrames
-	df_list = [pd.read_html(base_html.format(year)) for year in year_list]
+	df_list = [get_season(base_html.format(year)) for year in year_list]
 
+	# Create list of unsuccessful downloads
+	df_fail = [check for check in df_list if check is None]
+
+	# Verify all downloads were successful
+	try:
+		assert(len(df_fail) == 0)
+	except AssertionError as err:
+		logger.exception("Missing {} datasets".format(len(df_fail)))
+		raise err
+	
 	# Stack data
 	df = pd.concat(df_list, ignore_index = True)
 
-    # diagnostic prints
-	df.info()
-	df.head(n=10)
+	# diagnostic prints
+	logger.info("Dimensions of {} are {}".format(outfile, df.shape))
 
 	# save DataFrame as .csv file
-	df.to_csv(outfile)
-
+	try:
+		df.to_csv(outfile)
+	except FileNotFoundError as err:
+		logger.exception("Error saving file {}".format(outfile))
+		raise err
+	else:
+		logger.info("{} created successfully".format(outfile))
 
 def main():
 	"""
@@ -42,39 +85,46 @@ def main():
 	and Football Outsiders and store as .csv files
 	"""
 
+	logger.info("Starting program execution")
+
+	# set path to output data
+	datapath = re.sub("/src/data", "/data/raw/{filename}", os.getcwd())
+	logger.info("Output files will be directed to {}".format(datapath))
+
+	# HTML path templates
+	pfr_path = "https://www.pro-football-reference.com/years/{}/passing.htm"
+	fo_path = "https://www.footballoutsiders.com/stats/qb{}"
+
+	# download Pro Football Reference data
+	get_all_seasons(2002,
+					2017,
+					pfr_path,
+					datapath.format(filename="qb_season_pfr.csv"))
+
+	# download Football Outsiders data
+	get_all_seasons(2002,
+					2017,
+					fo_path,
+					datapath.format(filename="qb_season_fo.csv"))
+
+	logger.info("End program execution")
+
+if __name__ == "__main__":
+
 	# set name of log file
 	log_filename = "download.log"
 
-	# set path to output data
-	datapath = re.sub("/src", "/data/raw/{filename}", os.getcwd())
+	# overwite any existing log file
+	if os.path.exists(log_filename):
+		os.remove(log_filename)
 
-	# set up logging
+	# set up logger
 	logging.basicConfig(filename=log_filename,
-		                filemode="w",
+						filemode="w",
 						level=logging.DEBUG,
 						format="%(levelname)s: %(asctime)s: %(message)s")
 
-	logging.info("Running {}".format(re.sub(".log", ".py", log_filename)))
+	logger = logging.getLogger(__name__)
 
-
-    # HTML path templates
-    pfr_path = "https://www.pro-football-reference.com/years/{}/passing.htm"
-    fo_path = "https://www.footballoutsiders.com/stats/qb{}"
-
-    # download data
-    get_all_seasons(2002,
-    	            2017,
-    	            pfr_path,
-    	            datapath.format(filename="qb_season_pfr.csv"))
-
-    
-    get_all_seasons(2002,
-    	            2017,
-    	            fo_path,
-    	            datapath.format(filename="qb_season_fo.csv"))
-
-    logging.info("Program Complete")
-
-if __name__ == "__main__":
+	# begin execution
 	main()
-
