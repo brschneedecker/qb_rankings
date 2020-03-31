5f8e5ca353c7936f15997f9feb746504ec31d4b0
Ben Schneedecker <brschneedecker@gmail.com>
2020-03-30 21:18:01 -0400

    add 'unit testing' program stub for qb ETL

---
+++ src/test_qb_etl.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Unit tests for qb_etl.py
+
+Note: This does not use a real unit testing framework. It's actually
+just sad ad hoc functions for testing stuff
+
+TODO: Update to use real unit testing framework
+
+@author: ben
+"""
+
