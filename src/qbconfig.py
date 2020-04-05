c21c697246335f194028ab9ef28a2677652c490a
Ben Schneedecker <brschneedecker@gmail.com>
2020-04-04 23:10:53 -0400

    Add exception handling and logging to data load

--- src/qbconfig.py
+++ src/qbconfig.py
@@ -4,8 +4,6 @@ This program contains globally used paths and names of external files used in pr
 TODO: Convert to YAML config
 """
 
-import datetime
-
 # HTML paths for data extraction
 pfr_base_html = "https://www.pro-football-reference.com/years/{year}/passing.htm"
 fo_base_html = "https://www.footballoutsiders.com/stats/qb{year}"
@@ -17,6 +15,7 @@ esf_xwalk = "xwalks/elite_system_fraud.csv"
 
 # Log files
 etl_log = "logs/qb_etl_{:%Y-%m-%d_%H:%M:%S}.log"
+db_create_log = "logs/db_create_{:%Y-%m-%d_%H:%M:%S}.log"
 
 # Database file
 db_file = "data/qb_sqlite.db"
